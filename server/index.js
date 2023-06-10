05328ae729df4cdb0a47f0106b7a6d5afb34dd3c
Kavyanjali Agnihotri <kavyagnihotri17@gmail.com>
2023-06-10 22:55:12 +0530

    add reqs apis

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let user_router = require('./routes/user.js');
 let feed_router = require('./routes/feed.js');
+let request_router = require('./routes/request.js');
 app.use('/user',user_router)
 app.use('/feed',feed_router)
-
+app.use('/request',request_router)
 
 app.listen(process.env.SERVER_PORT,process.env.SERVER_HOST,()=>{
     console.log(`Server is now running`)
