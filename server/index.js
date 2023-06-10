ad112e0c3eab62191c1f04b5bb67428c24dd699e
Parth Pratim Chatterjee <parth27official@gmail.com>
2023-06-11 05:09:32 +0530

    Static File Hosting

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config()
 
+
 const app = express()
+app.use(express.static('../static'))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- server/routes/request.js
+++ server/routes/request.js
@@ -51,7 +51,7 @@ router.post('/create', asyncHandler(async (req,res) => {
     try {
         const mongodb = new mongodbConnector();
         let reqs = await mongodb.connect("requests");
-    
+                                                                                               
         await reqs.insertOne({
           "tag": req.body.tag,
           "emergency": req.body.emergency,
