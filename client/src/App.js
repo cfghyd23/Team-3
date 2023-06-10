f2654d051d5285a7afe3c5f6815eae9c4998b8fe
Jatin Tank <jatin.tank.official@gmail.com>
2023-06-10 20:01:55 +0530

    Created Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Login } from './Components/Login/Login';
 import Main from "./Components/Main/Main"
+import { Register } from './Components/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <Login/>
+      <Register/>
     </div>
   );
 }
